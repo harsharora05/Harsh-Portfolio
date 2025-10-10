84c9e5a942d26a5a51a84499c8cd2539255ae5e9
harsharora05 <hararora12@gmail.com>
2025-10-11 00:19:02 +0530

    a simple path fix

--- components/BNavbar.tsx
+++ components/BNavbar.tsx
@@ -7,7 +7,7 @@ import { usePathname } from "next/navigation";
 import clsx from "clsx";
 
 export function BottomNavBar() {
-    const pathname = usePathname();
+    const pathname = usePathname() || '/';
     return <div className="z-10 flex  w-[200px] navmd:w-lg bg-white-200 rounded-full px-2 py-3 navmd:px-3 navmd:py-4 justify-around items-center  fixed bottom-6
     navmd:bottom-10  left-1/2 -translate-x-1/2 border border-gray-50">
         <Link href={"/"}><div className={clsx(pathname == '/' ? "bg-blue-200 px-3 py-2 navmd:px-5 navmd:py-3 rounded-full text-white" : "bg-none text-gray-400", "font-bold  flex gap-2 hover:pointer")} ><span><HomeIcon /></span><span className="hidden navmd:block">Home </span></div></Link>
